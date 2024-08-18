16181388404311cbe857507e1041fd4b73cee17d
Diome Nike Potot <diomenikep@gmail.com>
2024-08-18 13:18:26 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_TENANT_API,
+        hostname: process.env.NEXT_PUBLIC_API_DOMAIN,
         port: "",
         pathname: `/**`,
       },
