7963d6472d7e9c66ee2824e8e3f1f1745c9f44c9
localnetwork <diomenikep@gmail.com>
2024-09-22 21:18:35 +0800

    chore: removed framer imageStats, wp

--- components/blocks/ImageWithStats.jsx
+++ components/blocks/ImageWithStats.jsx
@@ -1,27 +1,24 @@
 import helper from "@/lib/helpers/helper";
-import Image from "next/image";
 import siteConfig from "@/site.config";
 import { motion, useInView } from "framer-motion";
+import Image from "next/image";
+
+const StatItem = ({ item }) => {
+  return (
+    <motion.div className="my-[50px] text-center">
+      <h2 className="leading-normal text-[#1B217A] font-secondary text-[60px]">
+        {item.Value}
+      </h2>
+      <p className="text-[#1e1e1e] font-bold mt-[15px]">{item.Label}</p>
+    </motion.div>
+  );
+};
 
 export default function ImageWithStats({ block }) {
   const { Color, Stats } = block;
   const { colorExtractor } = helper;
 
   const extractedColor = colorExtractor(Color);
-
-  const containerVariants = {
-    hidden: {},
-    visible: {
-      transition: {
-        staggerChildren: 0.2, // Stagger children by 0.2 seconds
-      },
-    },
-  };
-
-  const itemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0 },
-  };
 
   return (
     <section
@@ -37,33 +34,15 @@ export default function ImageWithStats({ block }) {
             className="object-cover w-full"
           />
         </div>
-        <motion.div
-          className="grid grid-cols-4 gap-[30px]"
-          variants={containerVariants}
-          initial="hidden"
-          whileInView="visible"
-          viewport={{ once: false }} // Set to false to trigger again when it comes into view
-        >
-          {Stats &&
-            Stats.map((item, index) => {
-              const { ref, inView } = useInView({ triggerOnce: true });
-              return (
-                <motion.div
-                  key={index}
-                  ref={ref}
-                  variants={itemVariants}
-                  className="my-[50px] text-center"
-                >
-                  <h2 className="leading-normal text-[#1B217A] font-secondary text-[60px]">
-                    {item.Value}
-                  </h2>
-                  <p className="text-[#1e1e1e] font-bold mt-[15px]">
-                    {item.Label}
-                  </p>
-                </motion.div>
-              );
-            })}
-        </motion.div>
+        {Stats && (
+          <>
+            <div className="grid grid-cols-4 gap-[30px]">
+              {Stats.map((item, index) => (
+                <StatItem key={index} item={item} />
+              ))}
+            </div>
+          </>
+        )}
       </div>
     </section>
   );
--- components/partials/FooterSocial.jsx
+++ components/partials/FooterSocial.jsx
@@ -29,6 +29,7 @@ export default function FooterSocial({ social_links }) {
       {social_links.map((item, index) => {
         return (
           <Link
+            key={index}
             className="hover:mt-[-5px] hover:opacity-80 transition-[all_ease_0.3s] inline-block "
             href={item?.Link || "#"}
           >
