b75d9041e2baa6537175826623ffc81b10cb0bbf
localnetwork <diomenikep@gmail.com>
2024-12-18 08:55:07 +0800

    disabled imageLoader

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
     unoptimized: process.env.CLOUDFLARE == 1 ? true : false, // Disable Image Optimization API
   },
   images: {
-    loader: process.env.CLOUDFLARE == 1 ? "custom" : "default",
-    loaderFile: "./components/partials/ImageLoader.jsx",
+    // loader: process.env.CLOUDFLARE == 1 ? "custom" : "default",
+    // loaderFile: "./components/partials/ImageLoader.jsx",
     remotePatterns: [
       {
         protocol: "https",
